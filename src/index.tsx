4640c4a67cfeb7736f8507a810fbc7cb9ef7b2e9
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-01-29 22:39:32 +0200

    Added redusers into app store in src/redux/store.ts;

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import App from "./components/App/App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,22 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+
+import columnReducer from "./slices/columnReducer";
+import taskReducer from "./slices/taskReducer";
+import userReducer from "./slices/userReducer";
 
 export const store = configureStore({
-    reducer: {
-        //reducers
-    },
+  reducer: {
+    user: userReducer,
+    column: columnReducer,
+    task: taskReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-    ReturnType,
-    RootState,
-    unknown,
-    Action<string>
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
 >;
