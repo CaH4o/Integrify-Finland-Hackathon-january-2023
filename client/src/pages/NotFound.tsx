874e3b12a87fa8a495dc7dc390ec42f896c729bf
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-01-30 21:08:42 +0200

    remove error and add style in NorFound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,19 +1,9 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { useNavigate, NavigateFunction } from "react-router-dom";
-import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function NotFound(): JSX.Element {
   const navigate: NavigateFunction = useNavigate();
-  const error: unknown = useRouteError();
-
-  if (error instanceof Error) {
-    console.log(error.message);
-  } else if (isRouteErrorResponse(error)) {
-    console.log(error.status, error.statusText);
-  } else {
-    console.log("Unexpected error: ", error);
-  }
 
   useEffect(function () {
     const timer = setTimeout(handleNavigate, 3000);
@@ -27,16 +17,24 @@ export default function NotFound(): JSX.Element {
   }
 
   return (
-    <Box component="div" sx={{ textAlign: "center" }}>
-      <Typography component="h1" variant="h1">
-        Oops!
-      </Typography>
-      <Typography component="p" variant="body2">
-        Sorry, the page is not found.
-      </Typography>
-      <Typography component="p" variant="body2">
-        You will be redirect on main page in 3 seconds.
-      </Typography>
+    <Box sx={{ width: "100%", height: "100vh", display: "table" }}>
+      <Box
+        sx={{
+          textAlign: "center",
+          verticalAlign: "middle",
+          display: "table-cell",
+        }}
+      >
+        <Typography component="h3" variant="h3">
+          Oops!
+        </Typography>
+        <Typography component="p" variant="body1" m="1rem">
+          Sorry, the page is not found.
+        </Typography>
+        <Typography component="p" variant="body2">
+          You will be redirect on the Home page in 3 seconds.
+        </Typography>
+      </Box>
     </Box>
   );
 }
