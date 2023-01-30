b77f4099b0c1354409aad52abbe2b29ab48fe434
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-30 08:54:57 +0100

    added sign in and sign up components with styling

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,36 @@
+import { Box, Icon, IconButton } from "@mui/material"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import "./SignIn.scss"
+
+const SignIn = () => {
+  return (
+    <Box className="signIn">
+        <h1 className="signIn__heading">Log In</h1>
+        <input type={"email"} className="singIn__email" placeholder="Email"/>
+        <input type={"password"} className="signIn__password" placeholder="Password"/>
+        <div className="rememberMe">
+        <input type="checkbox" name="remember me" id="remember" />
+        <label htmlFor="remember">Remember me</label>
+        </div>
+        <button className="login__btn">Log In</button>
+        <p className="forgot">Forgot password?</p>
+        <p className="or"><span>OR</span></p>
+        <Box className="links">
+        <IconButton disableRipple>
+          <LinkedInIcon className="link"/>
+        </IconButton>
+        <IconButton disableRipple>
+          <FacebookIcon className="link"/>
+        </IconButton>
+        <IconButton disableRipple>
+          <LockOpenIcon className="link"/>
+        </IconButton>
+        </Box>
+
+    </Box>
+  )
+}
+
+export default SignIn
