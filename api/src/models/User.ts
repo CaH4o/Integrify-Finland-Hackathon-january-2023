7c52c377f2c6860204d42a18a54175d031600d88
Ronja Pietrzykowska <ronja.pietrzykowska@integrify.io>
2023-01-31 11:54:27 +0100

    updated model and api

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,34 +1,63 @@
 import mongoose, { Document } from 'mongoose';
 
 export type UserDocument = Document & {
-  avatar: string;
-  name: string;
-  position: string;
+  nameFirst: string;
+  nameLast: string;
   email: string;
+  position: string;
+  company: string;
+  avatar: string;
+  phone: string;
+  bio: string;
   password: string;
 };
 
 const userSchema = new mongoose.Schema({
-  avatar: {
+  nameFirst: {
     type: String,
-  },
-  name: {
-    type: String,
-    minLength: [2, 'Name must be at least 2 characters'],
+    minLength: [2, 'First name must be at least 2 characters'],
+    maxLength: [50, 'First name must be at most 50 characters'],
     trim: true,
   },
-  position: {
+  nameLast: {
     type: String,
-    minLength: [2, 'Position must be at least 2 characters'],
+    minLength: [2, 'Last name must be at least 2 characters'],
+    maxLength: [50, 'Last name must be at most 50 characters'],
     trim: true,
   },
   email: {
     type: String,
     required: [true, 'Email is required'],
+    maxLength: [50, 'Email must be at most 50 characters'],
     unique: true,
     lowercase: true,
   },
-
+  position: {
+    type: String,
+    minLength: [2, 'Position must be at least 2 characters'],
+    maxLength: [100, 'Position must be at most 100 characters'],
+    trim: true,
+  },
+  company: {
+    type: String,
+    minLength: [2, 'Company must be at least 2 characters'],
+    maxLength: [100, 'Company must be at most 100 characters'],
+    trim: true,
+  },
+  avatar: {
+    type: String,
+  },
+  phone: {
+    type: String,
+    minLength: [6, 'Phone number must be at least 6 characters'],
+    maxLength: [20, 'Phone number must be at most 20 characters'],
+    trim: true,
+  },
+  bio: {
+    type: String,
+    maxLength: [500, 'Bio must be at most 500 characters'],
+    trim: true,
+  },
   password: {
     type: String,
     required: [true, 'Password is required'],
